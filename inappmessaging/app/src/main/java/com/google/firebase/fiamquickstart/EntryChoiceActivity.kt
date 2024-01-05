9d7c350617632ba99fc5ec33073ca9076aee0022
Ranjith T <ranjith@bytemark.co>
2024-01-05 11:50:56 +0530

    Modified the app to test banner type alerts reappearing in child activity issue

--- inappmessaging/app/src/main/java/com/google/firebase/fiamquickstart/EntryChoiceActivity.kt
+++ inappmessaging/app/src/main/java/com/google/firebase/fiamquickstart/EntryChoiceActivity.kt
@@ -1,12 +1,41 @@
 package com.google.firebase.fiamquickstart
 
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.firebase.example.internal.BaseEntryChoiceActivity
 import com.firebase.example.internal.Choice
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
 import com.google.firebase.fiamquickstart.java.MainActivity
 import com.google.firebase.fiamquickstart.kotlin.KotlinMainActivity
+import com.google.firebase.inappmessaging.FirebaseInAppMessaging
+import com.google.firebase.inappmessaging.inAppMessaging
+import com.google.firebase.installations.installations
+import com.google.firebase.fiamquickstart.R
 
 class EntryChoiceActivity : BaseEntryChoiceActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private lateinit var firebaseIam: FirebaseInAppMessaging
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
+        firebaseIam = Firebase.inAppMessaging
+
+        firebaseIam.isAutomaticDataCollectionEnabled = true
+        firebaseIam.setMessagesSuppressed(false)
+
+
+        findViewById<Button>(com.google.firebase.inappmessaging.display.R.id.button).setOnClickListener {
+            firebaseAnalytics.logEvent("engagement_party_two", Bundle())
+        }
+
+    }
 
     override fun getChoices(): List<Choice> {
         return listOf(
